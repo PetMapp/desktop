e0994bc7001e05338679c87635ba786ecf6b0810
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-06-05 19:09:05 -0300

    ✨feat: implementa dialog

--- desktop/src/app/components/floating/floating.component.ts
+++ desktop/src/app/components/floating/floating.component.ts
@@ -1,9 +1,17 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { ButtonIconComponent } from '../iconButton/iconButton.component';
 
+import { BrnDialogModule } from '@spartan-ng/brain/dialog';
+import { HlmDialogModule } from '@spartan-ng/helm/dialog';
+
 @Component({
   selector: 'app-floating',
-  imports: [ButtonIconComponent],
+  standalone: true,
+  imports: [
+    ButtonIconComponent,
+    BrnDialogModule,
+    HlmDialogModule,
+  ],
   templateUrl: './floating.component.html',
   styleUrl: './floating.component.scss'
 })
--- desktop/src/app/components/iconButton/iconButton.component.ts
+++ desktop/src/app/components/iconButton/iconButton.component.ts
@@ -1,8 +1,8 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, forwardRef } from '@angular/core';
 import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 import { HlmSpinnerComponent } from '@spartan-ng/ui-spinner-helm';
 import { HlmIconDirective } from '@spartan-ng/ui-icon-helm';
-import { provideIcons } from '@ng-icons/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
 import {
   lucideChevronRight,
   lucideMenu,
@@ -10,24 +10,35 @@ import {
   lucideHouse,
   lucideDog,
   lucideSearch,
-  lucideLocate,
-  lucideCamera,
   lucideLocateFixed,
-  lucideBell
+  lucideCamera,
+  lucideBell,
 } from '@ng-icons/lucide';
-import { NgIcon } from '@ng-icons/core';
+import { BrnDialogTriggerDirective } from '@spartan-ng/brain/dialog';
 
 @Component({
   selector: 'spartan-button-icon',
   standalone: true,
-  imports: [
-    HlmButtonDirective,
-    HlmSpinnerComponent,
-    HlmIconDirective,
-    NgIcon,
+  hostDirectives: [
+    {
+      directive: BrnDialogTriggerDirective,
+      inputs: [],
+      outputs: [],
+    },
   ],
+  imports: [HlmButtonDirective, HlmSpinnerComponent, HlmIconDirective, NgIcon],
   providers: [
-    provideIcons({ lucideChevronRight, lucideMenu, lucideCircleUserRound, lucideHouse, lucideDog, lucideSearch, lucideLocateFixed, lucideCamera, lucideBell }),
+    provideIcons({
+      lucideChevronRight,
+      lucideMenu,
+      lucideCircleUserRound,
+      lucideHouse,
+      lucideDog,
+      lucideSearch,
+      lucideLocateFixed,
+      lucideCamera,
+      lucideBell,
+    }),
   ],
   template: `
     <button
@@ -42,22 +53,11 @@ import { NgIcon } from '@ng-icons/core';
   `,
 })
 export class ButtonIconComponent {
-  /** Nome do ícone a ser exibido */
   @Input() iconName: string = 'lucideChevronRight';
-
-  /** Botão ativo (para estilos customizados) */
   @Input() isActive = false;
-
-  /** Variante do botão (padrão: secondary) */
   @Input() variant: 'default' | 'destructive' | 'outline' | 'secondary' | 'ghost' | 'link' | null = 'secondary';
-
-  /** Tamanho do botão (padrão: sm) */
   @Input() size: 'default' | 'sm' | 'lg' | 'icon' | null = 'sm';
-
-  /** Tamanho do ícone interno (padrão: sm) */
   @Input() iconSize: 'sm' | 'md' | 'lg' | string = 'sm';
-
-  /** Classe extra para estilização (ex.: posicionamento) */
   @Input() customClass = '';
 
   get buttonSize(): 'default' | 'sm' | 'lg' | 'icon' {

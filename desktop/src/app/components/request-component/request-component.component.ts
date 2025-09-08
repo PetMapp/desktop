4244d849dd5caa6fa847a796b1ff7f161ddfc1ec
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-09-08 14:25:00 -0300

    ✨feat: componente de diálogo de alerta e diretivas para tratamento de requisições

--- desktop/src/app/components/request-component/request-component.component.ts
+++ desktop/src/app/components/request-component/request-component.component.ts
@@ -1,60 +1,83 @@
-  import { Component, OnInit } from '@angular/core';
-  import { CommonModule } from '@angular/common';
-  import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
-  import {
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import {
+  HlmTabsComponent,
+  HlmTabsListComponent,
+  HlmTabsTriggerDirective,
+  HlmTabsContentDirective
+} from '@spartan-ng/helm/tabs';
+
+import { RequestService, PetRequest } from '../../services/request.service';
+import { AuthService } from '../../services/auth.service';
+import { translateStatus } from '../../utils/status-utils';
+import { formatDateBR } from '../../utils/date';
+import { IconComponent } from '../icon-component/icon-component.component';
+import { BrnAlertDialogContentDirective, BrnAlertDialogTriggerDirective } from '@spartan-ng/brain/alert-dialog';
+import {
+  HlmAlertDialogActionButtonDirective,
+  HlmAlertDialogCancelButtonDirective,
+  HlmAlertDialogComponent,
+  HlmAlertDialogContentComponent,
+  HlmAlertDialogDescriptionDirective,
+  HlmAlertDialogFooterComponent,
+  HlmAlertDialogHeaderComponent,
+  HlmAlertDialogOverlayDirective,
+  HlmAlertDialogTitleDirective,
+} from '@spartan-ng/helm/alert-dialog';
+
+@Component({
+  selector: 'app-request-component',
+  standalone: true,
+  imports: [
+    CommonModule,
+    HlmButtonDirective,
     HlmTabsComponent,
     HlmTabsListComponent,
     HlmTabsTriggerDirective,
-    HlmTabsContentDirective
-  } from '@spartan-ng/helm/tabs';
+    HlmTabsContentDirective,
+    IconComponent,
+    BrnAlertDialogTriggerDirective,
+    BrnAlertDialogContentDirective,
+    HlmAlertDialogComponent,
+    HlmAlertDialogContentComponent,
+    HlmAlertDialogOverlayDirective,
+    HlmAlertDialogHeaderComponent,
+    HlmAlertDialogTitleDirective,
+    HlmAlertDialogDescriptionDirective,
+    HlmAlertDialogFooterComponent,
+    HlmAlertDialogActionButtonDirective,
+    HlmAlertDialogCancelButtonDirective
+  ],
+  templateUrl: './request-component.component.html',
+  styleUrl: './request-component.component.scss'
+})
+export class RequestComponentComponent implements OnInit {
+  translateStatus = translateStatus;
+  formatDateBR = formatDateBR;
+  myRequests: PetRequest[] = [];
+  requestsForMyPets: PetRequest[] = [];
 
-  import { RequestService, PetRequest } from '../../services/request.service';
-  import { AuthService } from '../../services/auth.service';
-  import { translateStatus } from '../../utils/status-utils';
-  import { formatDateBR } from '../../utils/date';
-  import { IconComponent } from '../icon-component/icon-component.component';
+  loading = true;
 
-  @Component({
-    selector: 'app-request-component',
-    standalone: true,
-    imports: [
-      CommonModule,
-      HlmButtonDirective,
-      HlmTabsComponent,
-      HlmTabsListComponent,
-      HlmTabsTriggerDirective,
-      HlmTabsContentDirective,
-      IconComponent
-    ],
-    templateUrl: './request-component.component.html',
-    styleUrl: './request-component.component.scss'
-  })
-  export class RequestComponentComponent implements OnInit {
-    translateStatus = translateStatus;
-    formatDateBR = formatDateBR;
-    myRequests: PetRequest[] = [];
-    requestsForMyPets: PetRequest[] = [];
+  constructor(
+    private requestService: RequestService,
+    private auth: AuthService
+  ) { }
 
-    loading = true;
+  ngOnInit(): void {
+    this.auth.getUserLogged().subscribe(user => {
+      if (!user) return;
 
-    constructor(
-      private requestService: RequestService,
-      private auth: AuthService
-    ) { }
-
-    ngOnInit(): void {
-      this.auth.getUserLogged().subscribe(user => {
-        if (!user) return;
-
-        this.requestService.getRequestsByUser(user.uid).subscribe(res => {
-          this.myRequests = res.data ?? [];
-          console.log('Minhas solicitações:', this.myRequests);
-        });
-
-        this.requestService.getRequestsForUserPet(user.uid).subscribe(res => {
-          this.requestsForMyPets = res.data ?? [];
-          this.loading = false;
-        });
+      this.requestService.getRequestsByUser(user.uid).subscribe(res => {
+        this.myRequests = res.data ?? [];
+        console.log('Minhas solicitações:', this.myRequests);
       });
-    }
+
+      this.requestService.getRequestsForUserPet(user.uid).subscribe(res => {
+        this.requestsForMyPets = res.data ?? [];
+        this.loading = false;
+      });
+    });
   }
+}

b9f4d7b4d67f76a7c9ca50a4082565680c6f04dc
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-09-22 19:28:47 -0300

    ✨feat: mapa landing page

--- desktop/src/app/landing-page/landing-page.component.ts
+++ desktop/src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MapetsIconComponent } from '../components/mapets-icon/mapets-icon.component';
+import { MapViewComponent } from '../components/map-view/map-view.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MapetsIconComponent],
+  imports: [MapetsIconComponent, MapViewComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })

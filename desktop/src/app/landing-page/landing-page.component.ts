46a441867270194a65436eac469fa0d32b14b279
Gustavo Graciano <66232093+gustahh@users.noreply.github.com>
2025-10-01 11:02:52 -0300

    ✨feat: header mobile landing page

--- desktop/src/app/landing-page/landing-page.component.ts
+++ desktop/src/app/landing-page/landing-page.component.ts
@@ -3,15 +3,51 @@ import { MapetsIconComponent } from '../components/mapets-icon/mapets-icon.compo
 import { MapViewComponent } from '../components/map-view/map-view.component';
 import { Router } from '@angular/router';
 import { Redirection } from '../utils/redirection';
+import { viewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {
+  BrnSheetComponent,
+  BrnSheetContentDirective,
+  BrnSheetTriggerDirective
+} from '@spartan-ng/brain/sheet';
+import {
+  HlmSheetComponent,
+  HlmSheetContentComponent,
+  HlmSheetDescriptionDirective,
+  HlmSheetFooterComponent,
+  HlmSheetHeaderComponent,
+  HlmSheetTitleDirective
+} from '@spartan-ng/ui-sheet-helm';
+
+import { IconComponent } from '../components/icon-component/icon-component.component';
+import { Icon } from 'leaflet';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [MapetsIconComponent, MapViewComponent],
+  imports: [
+    MapetsIconComponent,
+    MapViewComponent,
+    BrnSheetComponent,
+    BrnSheetTriggerDirective,
+    BrnSheetContentDirective,
+    HlmSheetComponent,
+    HlmSheetHeaderComponent,
+    HlmSheetTitleDirective,
+    HlmSheetDescriptionDirective,
+    HlmSheetContentComponent,
+    HlmSheetFooterComponent,
+    IconComponent,
+    CommonModule
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
   private redirection: Redirection;
+  public readonly viewchildSheetRef = viewChild(HlmSheetComponent);
+  public isSheetOpen = false;
+
   constructor(private router: Router) {
     this.redirection = new Redirection(this.router);
   }
@@ -22,5 +58,19 @@ export class LandingPageComponent {
 
   goToRegister(): void {
     this.redirection.goTo('/register');
+  }
+
+
+  onSheetChange(open: Event) {
+    this.isSheetOpen = !!open;
+  }
+
+  closeSheet() {
+    this.viewchildSheetRef()?.close({});
+    this.isSheetOpen = false;
+  }
+
+  openSheet() {
+    this.isSheetOpen = true;
   }
 }
